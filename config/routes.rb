8dd678915e0025c729d0f10c485eae217cd63e02
Steven Torrence <storrence88@gmail.com>
2019-08-10 14:32:44 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'home#index'
-  post 'verifications#create'
+  post '/verifications', to: 'verifications#create'
 end
